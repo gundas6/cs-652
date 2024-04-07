ebf8f4d08ed7bbeecc7cb21284ab43b1eafcf35c
unknown <gundas6@mail.sacredheart.edu>
2024-04-06 20:56:55 -0400

    first commit

---
+++ c1bda.R
@@ -0,0 +1,39 @@
+#math
+v1 <-4.5
+v2 <-2
+v3 <-v1/v2
+v3
+
+#concate 
+greet <-"hello,"
+name <-"bob"
+message <-paste(greet, name)
+message
+
+#interger 
+x <-2L
+typeof(x)
+
+#logic
+result<-4<5
+result
+
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+
+
+#basicmath
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+
