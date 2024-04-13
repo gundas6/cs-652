f067fb237a055d0dea3d13d03c65f06d9c25c745
gundas6 <166187378+gundas6@users.noreply.github.com>
2024-04-12 23:38:14 -0400

    Add files via upload

---
+++ asg 1.R
@@ -0,0 +1,38 @@
+#Write a program to get the average of 3 numbers: 7, 12, and 5.
+A <- 7
+B <- 12
+C <- 5
+D <- A+B+C
+AVG <- D/3
+AVG
+
+
+#Write a program to print out “Welcome to R, <your first name>”
+
+A <- "WELCOME TO R"
+B <- "SAIKIRAN"
+C <- paste(A ,B)
+C
+
+
+#Let x be 7, y be 12 and z be 4, print out whether x is bigger than y and whether x is bigger than z
+
+x <- 7
+y <- 12
+z <- 4
+
+#  if x is bigger than y
+if (x > y) {
+  print("x is bigger than y")
+} else {
+  print("x is not bigger than y")
+}
+
+# if x is bigger than z
+if (x > z) {
+  print("x is bigger than z")
+} else {
+  print("x is not bigger than z")
+}
+
+
